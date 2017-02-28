1231c9469e032f64911d13f16d8a6fc8bf213b3d
okabi <okabi13@yahoo.co.jp>
2017-02-28 19:24:40 +0900

    handle emoji

--- main.rb
+++ main.rb
@@ -9,6 +9,17 @@ def log(log_path, text)
 end
 
 
+def shuffle(text)
+  emoji_regexp = /:[\w-+]+:/
+  emojis = text.scan(emoji_regexp)
+  ret = text.clone
+  emojis.each { |e| ret.slice!(e) }
+  ret = ret.split('')
+  emojis.each { |e| ret.push(' ' + e + ' ') }
+  return ret.shuffle.join
+end
+
+
 log_path = Time.now.strftime("%Y%m%d%H%M%S") + '.log'
 begin
   # Initialize
@@ -21,12 +32,13 @@ begin
   client.on :message do |data|
     next if data.key?('bot_id')
     next if data['channel'] != Define::ChinkoChannelID
+    text = data['text']
     params = {
       token: Define::WebToken,
       channel: Define::ChinkoChannelID,
       username: 'コンちゃん',
       icon_url: Define::BotIconURL,
-      text: data['text'].split('').shuffle.join,
+      text: shuffle(data['text']),
     }
     Slack.chat_postMessage params
   end
